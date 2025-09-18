49df5c7ff08d7061ed18e323e6328832b25bea26
Shubham Pandey <shubhamjanki000@gmail.com>
2025-09-18 21:17:43 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    <h1>hello world</h1>
+    <Button>click me</Button>
+    </div>
+  );
+}
